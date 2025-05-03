105e8400f74570150c9df9b9a35b80a0058062b4
Faruk Spahic <farukspahictz@gmail.com>
2025-05-03 16:45:45 +0200

    Added reusable dropdown

---
+++ src/components/ui/Dropdown.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { ReactNode, useState } from "react";
+import { cn } from "@/lib/utils";
+import { MdOutlineKeyboardArrowDown } from "react-icons/md"; // Default icon import
+
+interface DropdownProps {
+  label: string;
+  children: ReactNode;
+  className?: string;
+}
+
+export function Dropdown({ label, children, className }: DropdownProps) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  return (
+    <div className="w-full">
+      <button
+        type="button"
+        className={cn("flex space-x-2 items-center cursor-pointer", className)}
+        onClick={handleToggle}
+      >
+        <span className="font-semibold">{label}</span>
+        <MdOutlineKeyboardArrowDown
+          className={cn("text-xl transition-transform", {
+            "rotate-180": !isOpen,
+          })}
+        />
+      </button>
+
+      {isOpen && <>{children}</>}
+    </div>
+  );
+}
