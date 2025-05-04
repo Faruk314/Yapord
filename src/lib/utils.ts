7ed469f838609ffa04907923f7d5332b6ec50b9c
Faruk Spahic <farukspahictz@gmail.com>
2025-05-04 19:59:39 +0200

    Added utility method for creating file names

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,18 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { v4 as uuidv4 } from "uuid";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+function createUniqueFileNames(images: File[]) {
+  const fileNames = images.map((image) => {
+    const fileName = `${uuidv4()}-${image.name}`;
+    return fileName;
+  });
+
+  return fileNames;
+}
+
+export { createUniqueFileNames };
