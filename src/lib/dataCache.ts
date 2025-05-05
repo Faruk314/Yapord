cfb064b68dbd8abc732b4dfbc7c360c3c88fe553
Faruk Spahic <farukspahictz@gmail.com>
2025-05-05 19:22:35 +0200

    Added global tags for cache

---
+++ src/lib/dataCache.ts
@@ -0,0 +1,19 @@
+type CACHE_TAG = "users" | "servers";
+
+function getGlobalTag(tag: CACHE_TAG) {
+  return `global:${tag}` as const;
+}
+
+function getIdTag(tag: CACHE_TAG, id: string) {
+  return `id:${id}-${tag}` as const;
+}
+
+function getUserTag(tag: CACHE_TAG, userId: string) {
+  return `user:${userId}-${tag}` as const;
+}
+
+function getServerTag(tag: CACHE_TAG, serverId: string) {
+  return `server:${serverId}-${tag}` as const;
+}
+
+export { getGlobalTag, getIdTag, getUserTag, getServerTag };
