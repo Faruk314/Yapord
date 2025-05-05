fe0f9724e127323244da8dc16f1842b1b968da63
Faruk Spahic <farukspahictz@gmail.com>
2025-05-05 19:47:26 +0200

    Added custom alert dialog

---
+++ src/components/ui/AlertDialog.tsx
@@ -0,0 +1,63 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { ReactNode, useState } from "react";
+
+interface AlertDialogBoxProps {
+  trigger: ReactNode;
+  title: string;
+  description: string;
+  onConfirm: () => void | Promise<void>;
+  cancelText?: string;
+  confirmText?: string;
+}
+
+export function AlertDialogBox({
+  trigger,
+  title,
+  description,
+  onConfirm,
+  cancelText = "Cancel",
+  confirmText = "Continue",
+}: AlertDialogBoxProps) {
+  const [loading, setLoading] = useState(false);
+
+  const handleConfirm = async () => {
+    try {
+      setLoading(true);
+      await onConfirm();
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>{trigger}</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{title}</AlertDialogTitle>
+          <AlertDialogDescription>{description}</AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel disabled={loading}>{cancelText}</AlertDialogCancel>
+          <AlertDialogAction
+            className="bg-pink-600 hover:bg-pink-500 font-black"
+            onClick={handleConfirm}
+            disabled={loading}
+          >
+            {loading ? "Processing..." : confirmText}
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
