5256ced437ce7b1d2e9a315ad1b5298f9dc3334a
Faruk Spahic <farukspahictz@gmail.com>
2025-05-05 19:31:13 +0200

    Added server member zod schema

---
+++ src/features/servers/schemas/serverMembers.ts
@@ -0,0 +1,8 @@
+import { serverRoles } from "@/drizzle/schema/serverMember";
+import { z } from "zod";
+
+export const serverMemberSchema = z.object({
+  userId: z.string().uuid(),
+  serverId: z.string().uuid(),
+  role: z.enum(serverRoles).default("member"),
+});
