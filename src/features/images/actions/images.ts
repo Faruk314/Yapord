25e75b835b7a7bfd2816e9652ab6f4fa1d60d59d
Faruk Spahic <farukspahictz@gmail.com>
2025-05-05 19:20:47 +0200

    Added delete images action

---
+++ src/features/images/actions/images.ts
@@ -0,0 +1,28 @@
+"use server";
+
+import { db } from "@/drizzle/db";
+import { deleteServerImage } from "../../servers/db/servers";
+import { deleteImagesFromMinio } from "../db/images";
+
+async function deleteImages({
+  serverId,
+  images,
+}: {
+  serverId?: string;
+  images: string[];
+  userId?: string;
+}) {
+  try {
+    await db.transaction(async (trx) => {
+      if (serverId) await deleteServerImage(serverId, trx);
+
+      await deleteImagesFromMinio(images);
+    });
+
+    return { error: false, message: "Successfully deleted" };
+  } catch {
+    return { error: true, message: "Failed to delete" };
+  }
+}
+
+export { deleteImages };
