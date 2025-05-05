ee128f765e72b0e20aa71d681be591927ef56fa0
Faruk Spahic <farukspahictz@gmail.com>
2025-05-05 19:38:39 +0200

    Modified avatar component

--- src/components/ui/Avatar.tsx
+++ src/components/ui/Avatar.tsx
@@ -1,26 +1,32 @@
 import React from "react";
 import Image from "next/image";
-import { cn } from "@/lib/utils";
+import { cn, createMinioImageUrl } from "@/lib/utils";
 
 interface AvatarProps {
   imageSrc?: string | null;
+  name?: string;
   className?: string;
 }
 
-export default function Avatar({ imageSrc, className }: AvatarProps) {
+export default function Avatar({ imageSrc, className, name }: AvatarProps) {
   return (
     <span
       className={cn(
         "relative w-12 h-12 rounded-full overflow-hidden",
-        "bg-pink-600 text-white font-black uppercase",
+        "text-white font-black uppercase bg-pink-600 shadow-md",
         "flex items-center justify-center",
         className
       )}
     >
       {imageSrc ? (
-        <Image src={imageSrc} alt="" fill className="object-cover" />
+        <Image
+          src={createMinioImageUrl(imageSrc)}
+          alt={`images`}
+          fill={true}
+          className="bg-white"
+        />
       ) : (
-        <span className="text-xl z-10">F</span>
+        <span className="text-xl z-10">{name?.slice(0, 1)}</span>
       )}
     </span>
   );
