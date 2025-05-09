78dc2aab15ca91e99d003c0761436d0dca2150cc
Faruk Spahic <farukspahictz@gmail.com>
2025-05-09 15:52:55 +0200

    Added update user action

--- src/features/auth/actions/user.ts
+++ src/features/auth/actions/user.ts
@@ -1,7 +1,16 @@
+"use server";
+
 import { getUserFromSession } from "../db/session";
 import { cache } from "react";
 import { redirect } from "next/navigation";
 import { getUser } from "../db/user";
+import { userSchema } from "../schemas/user";
+import { canUpdateUser } from "../permissions/user";
+import { z } from "zod";
+import { createUniqueFileNames } from "@/lib/utils";
+import { uploadImagesToMinio } from "@/features/images/db/images";
+import { updateUser as updateUserDb } from "../db/user";
+import { db } from "@/drizzle/db";
 
 type FullUser = Exclude<Awaited<ReturnType<typeof getUser>>, undefined | null>;
 
@@ -50,3 +59,50 @@ async function _getCurrentUser({
 }
 
 export const getCurrentUser = cache(_getCurrentUser);
+
+async function updateUser(
+  userId: string,
+  unsafeData: z.infer<typeof userSchema>
+) {
+  const { success, data } = userSchema.safeParse(unsafeData);
+
+  if (!success) {
+    return { error: true, message: "There was an error updating your profile" };
+  }
+
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return { error: true, message: "There was an error updating your profile" };
+  }
+
+  if (!canUpdateUser(currentUser.id, userId)) {
+    return { error: true, message: "There was an error updating your profile" };
+  }
+
+  try {
+    const imageUrls =
+      data.image && typeof data.image !== "string"
+        ? createUniqueFileNames([data.image])
+        : [];
+
+    const userData = {
+      ...data,
+      image: imageUrls[0] || null,
+    };
+
+    await db.transaction(async (trx) => {
+      await updateUserDb(userId, userData, trx);
+
+      if (data.image && typeof data.image !== "string") {
+        await uploadImagesToMinio([data.image], imageUrls);
+      }
+    });
+
+    return { error: false, message: "Successfully updated your profile" };
+  } catch {
+    return { error: true, message: "Unable to update your profile" };
+  }
+}
+
+export { updateUser };
