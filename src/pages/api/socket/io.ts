9bf520a1264d44528aeb4cf4b436d89ed1fe1414
Faruk Spahic <farukspahictz@gmail.com>
2025-05-14 20:05:13 +0200

    Added middleware in socket io and initialized channel listeners

--- src/pages/api/socket/io.ts
+++ src/pages/api/socket/io.ts
@@ -1,6 +1,11 @@
 import { Server as ServerIO, Socket } from "socket.io";
 import { NextApiResponseServerIO } from "@/types/next";
 import { NextApiRequest } from "next";
+import { getUserSessionById } from "@/features/auth/db/session";
+import ChannelListeners from "@/features/channels/webSocket/listeners/server/channel";
+import { db } from "@/drizzle/db";
+import { eq } from "drizzle-orm";
+import { UserTable } from "@/drizzle/schema";
 
 export const config = {
   api: {
@@ -17,14 +22,61 @@ function ioHandler(req: NextApiRequest, res: NextApiResponseServerIO) {
     const io = new ServerIO(httpServer, {
       path: path,
       addTrailingSlash: false,
+      cors: {
+        origin: "http://localhost:3000",
+        credentials: true,
+      },
     });
 
-    // io.use(async (socket: Socket, next) => {
-    //   const cookieHeader = socket.request.headers.cookie;
-    // });
+    io.use(async (socket: Socket, next) => {
+      const cookieHeader = socket.request.headers.cookie;
+
+      if (!cookieHeader) {
+        return next(new Error("Cookies authentication error"));
+      }
+
+      const cookies = Object.fromEntries(
+        cookieHeader.split("; ").map((c) => c.split("="))
+      );
+
+      const sessionId = cookies["session-id"];
+
+      if (!sessionId) {
+        return next(new Error("Cookies authentication error"));
+      }
+
+      const session = await getUserSessionById(sessionId);
+
+      if (!session) {
+        return next(new Error("Session authentication error"));
+      }
+
+      try {
+        const user = await db.query.UserTable.findFirst({
+          columns: {
+            id: true,
+            name: true,
+            image: true,
+          },
+          where: eq(UserTable.id, session.id),
+        });
+
+        if (!user)
+          return next(new Error("Could not get user in socket io middleware"));
+
+        socket.user = { id: user.id, name: user.name, image: user.image };
+
+        next();
+      } catch (err) {
+        console.error("Error fetching user in socket middleware:", err);
+        return next(new Error("Error fetching user data"));
+      }
+    });
 
     io.on("connection", (socket) => {
-      console.log("Connected:", socket.id);
+      const channelListeners = new ChannelListeners(io, socket);
+
+      channelListeners.registerListeners();
     });
 
     res.socket.server.io = io;
