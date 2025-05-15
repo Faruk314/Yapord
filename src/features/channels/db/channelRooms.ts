9034cfd25f182b5d3acc9d29ed6bb706ad20f004
Faruk Spahic <farukspahictz@gmail.com>
2025-05-15 18:45:23 +0200

    Changed channel rooms to  use redis hash methods

--- src/features/channels/db/channelRooms.ts
+++ src/features/channels/db/channelRooms.ts
@@ -5,28 +5,31 @@ import { IserverMember } from "@/features/servers/types/servers";
 const ROOMS_KEY = "channels";
 
 async function createChannelRoom(roomId: string) {
-  const roomExists = await redisClient.exists(`${ROOMS_KEY}:${roomId}`);
-
+  const roomExists = await redisClient.hexists(ROOMS_KEY, roomId);
   if (roomExists) throw new Error("Channel with this id already exists");
 
   try {
-    await redisClient.set(
-      `${ROOMS_KEY}:${roomId}`,
-      JSON.stringify({ roomId, users: [] })
-    );
+    const newRoom: IchannelRoom = { id: roomId, users: [] };
+    await redisClient.hset(ROOMS_KEY, roomId, JSON.stringify(newRoom));
   } catch {
     throw new Error("Failed to create channel room");
   }
 }
 
-async function joinChannelRoom(joinedUser: IserverMember, roomId: string) {
-  const roomJSON = await redisClient.get(`${ROOMS_KEY}:${roomId}`);
+async function getChannelRoom(roomId: string) {
+  const roomJSON = await redisClient.hget(ROOMS_KEY, roomId);
 
   if (!roomJSON) {
     throw new Error("Channel room does not exist");
   }
 
   const channelRoom: IchannelRoom = JSON.parse(roomJSON);
+
+  return channelRoom;
+}
+
+async function joinChannelRoom(joinedUser: IserverMember, roomId: string) {
+  const channelRoom = await getChannelRoom(roomId);
 
   const inChannelRoom = channelRoom.users.some(
     (user) => user.id === joinedUser.id
@@ -37,13 +40,26 @@ async function joinChannelRoom(joinedUser: IserverMember, roomId: string) {
   channelRoom.users.push(joinedUser);
 
   try {
-    await redisClient.set(
-      `${ROOMS_KEY}:${roomId}`,
-      JSON.stringify(channelRoom)
-    );
+    await redisClient.hset(ROOMS_KEY, roomId, JSON.stringify(channelRoom));
   } catch {
     throw new Error("Failed to join channel room");
   }
 }
 
-export { createChannelRoom, joinChannelRoom };
+async function leaveChannelRoom(roomId: string, userId: string) {
+  const channelRoom = await getChannelRoom(roomId);
+
+  const inChannelRoom = channelRoom.users.some((user) => user.id === userId);
+
+  if (!inChannelRoom) throw new Error("User is not in this channel");
+
+  channelRoom.users = channelRoom.users.filter((user) => user.id !== userId);
+
+  try {
+    await redisClient.hset(ROOMS_KEY, roomId, JSON.stringify(channelRoom));
+  } catch {
+    throw new Error("Failed to leave channel room");
+  }
+}
+
+export { createChannelRoom, joinChannelRoom, leaveChannelRoom };
