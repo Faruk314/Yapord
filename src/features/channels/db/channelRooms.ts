8a94c8429a4f0efd0e53825994b4a73e5c15db27
Faruk Spahic <farukspahictz@gmail.com>
2025-05-15 15:50:56 +0200

    Added join channel room redis db method

--- src/features/channels/db/channelRooms.ts
+++ src/features/channels/db/channelRooms.ts
@@ -1,4 +1,6 @@
 import { redisClient } from "@/redis/redis";
+import { IchannelRoom } from "../types/channel";
+import { IserverMember } from "@/features/servers/types/servers";
 
 const ROOMS_KEY = "channels";
 
@@ -10,11 +12,38 @@ async function createChannelRoom(roomId: string) {
   try {
     await redisClient.set(
       `${ROOMS_KEY}:${roomId}`,
-      JSON.stringify({ roomId, users: {} })
+      JSON.stringify({ roomId, users: [] })
     );
   } catch {
     throw new Error("Failed to create channel room");
   }
 }
 
-export { createChannelRoom };
+async function joinChannelRoom(joinedUser: IserverMember, roomId: string) {
+  const roomJSON = await redisClient.get(`${ROOMS_KEY}:${roomId}`);
+
+  if (!roomJSON) {
+    throw new Error("Channel room does not exist");
+  }
+
+  const channelRoom: IchannelRoom = JSON.parse(roomJSON);
+
+  const inChannelRoom = channelRoom.users.some(
+    (user) => user.id === joinedUser.id
+  );
+
+  if (inChannelRoom) throw new Error("User is already in this channel");
+
+  channelRoom.users.push(joinedUser);
+
+  try {
+    await redisClient.set(
+      `${ROOMS_KEY}:${roomId}`,
+      JSON.stringify(channelRoom)
+    );
+  } catch {
+    throw new Error("Failed to join channel room");
+  }
+}
+
+export { createChannelRoom, joinChannelRoom };
