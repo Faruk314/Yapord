5f934c6d8a0032aa6b95d41bc6d65317d8866d89
Faruk Spahic <farukspahictz@gmail.com>
2025-05-17 14:45:32 +0200

    Refactored server layout method

--- src/app/(private)/server/[serverId]/layout.tsx
+++ src/app/(private)/server/[serverId]/layout.tsx
@@ -1,13 +1,9 @@
 import React from "react";
-import { HiMiniHashtag, HiOutlineSpeakerWave } from "react-icons/hi2";
-import { Dropdown } from "@/components/dropdowns/Dropdown";
 import { getServer } from "@/features/servers/db/servers";
 import { ServerDropdownMenu } from "@/features/servers/components/ServerDropdownMenu";
-import { getCurrentUser } from "@/features/auth/actions/user";
 import { redirect } from "next/navigation";
 import { getCurrentServerMember } from "@/features/servers/actions/serverMembers";
-import CreateChannel from "@/features/channels/components/modals/CreateChannel";
-import Link from "next/link";
+import Channels from "@/features/channels/components/Channels";
 
 export default async function ServerLayout({
   params,
@@ -16,8 +12,6 @@ export default async function ServerLayout({
   params: Promise<{ serverId: string }>;
   children: React.ReactNode;
 }) {
-  const user = await getCurrentUser({ redirectIfNotFound: true });
-
   const { serverId } = await params;
 
   const server = await getServer(serverId);
@@ -34,54 +28,12 @@ export default async function ServerLayout({
 
           <ServerDropdownMenu
             serverMemberRole={serverMember?.role}
-            ownerId={user.id}
+            ownerId={serverMember.userId}
             server={server}
           />
         </div>
 
-        <div className="p-4 flex items-start">
-          <Dropdown label="Text channel">
-            <div className="pt-4">
-              <div className="flex flex-col space-y-1">
-                {server.channels
-                  .filter((c) => c.type === "text")
-                  .map((channel) => (
-                    <Link
-                      href={`/server/${server.id}/${channel.id}`}
-                      key={channel.id}
-                      className="flex space-x-2 items-center text-gray-500 py-1 hover:bg-gray-100 hover:rounded-md cursor-pointer"
-                    >
-                      <HiMiniHashtag />
-                      <span>{channel.name}</span>
-                    </Link>
-                  ))}
-              </div>
-            </div>
-          </Dropdown>
-
-          <CreateChannel serverId={serverId} channelType="text" />
-        </div>
-
-        <div className="px-4 pb-4 flex items-start">
-          <Dropdown label="Voice channel">
-            <div className="flex flex-col space-y-1 pt-4">
-              {server.channels
-                .filter((c) => c.type === "voice")
-                .map((channel) => (
-                  <Link
-                    href={`/server/${server.id}/${channel.id}`}
-                    key={channel.id}
-                    className="flex space-x-2 items-center text-gray-500 py-1 hover:bg-gray-100 hover:rounded-md cursor-pointer"
-                  >
-                    <HiOutlineSpeakerWave />
-                    <span>{channel.name}</span>
-                  </Link>
-                ))}
-            </div>
-          </Dropdown>
-
-          <CreateChannel serverId={serverId} channelType="voice" />
-        </div>
+        <Channels serverId={serverId} />
       </div>
 
       {children}
