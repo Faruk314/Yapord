101477f5f03c7aa0d819c2c2b62d548010350df2
Faruk Spahic <farukspahictz@gmail.com>
2025-05-19 22:01:03 +0200

    Refactored friends navigation component

--- src/components/navigation/FriendsNavigation.tsx
+++ src/components/navigation/FriendsNavigation.tsx
@@ -1,28 +1,28 @@
+"use client";
+
 import { FaUserFriends } from "react-icons/fa";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import classNames from "classnames";
 
 const navItems = [
   {
-    href: "/home/friends/online",
+    href: "/home/friends",
     label: "Online",
-    className:
-      "bg-transparent hover:bg-gray-100 cursor-pointer text-sm p-2 rounded-md font-black uppercase shadow-none text-black",
   },
   {
     href: "/home/friends/all",
     label: "All",
-    className:
-      "bg-transparent hover:bg-gray-100 cursor-pointer text-sm p-2 rounded-md font-black uppercase shadow-none text-black",
   },
   {
     href: "/home/friends/add",
     label: "Add friend",
-    className:
-      "bg-pink-600 p-2 text-white text-sm hover:bg-pink-400 rounded-md cursor-pointer font-black uppercase",
   },
 ];
 
 export default function FriendsNavigation() {
+  const pathname = usePathname();
+
   return (
     <div className="flex items-center space-x-4 h-18 px-4 border-b border-gray-300">
       <div className="flex items-center space-x-2">
@@ -30,8 +30,17 @@ export default function FriendsNavigation() {
         <span className="text-sm font-black uppercase">friends</span>
       </div>
 
-      {navItems.map(({ href, label, className }) => (
-        <Link href={href} key={href} className={className}>
+      {navItems.map(({ href, label }) => (
+        <Link
+          key={href}
+          href={href}
+          className={classNames(
+            "text-sm p-2 rounded-md font-black uppercase cursor-pointer",
+            {
+              "bg-gray-100 text-black": pathname === href,
+            }
+          )}
+        >
           {label}
         </Link>
       ))}
