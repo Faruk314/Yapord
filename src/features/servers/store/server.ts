39490fdddeb4dbc6a3558c2b086cdfab74a66e23
Faruk Spahic <farukspahictz@gmail.com>
2025-05-19 17:56:56 +0200

    Added action for getting server member in server store

--- src/features/servers/store/server.ts
+++ src/features/servers/store/server.ts
@@ -4,10 +4,16 @@ import { IserverMember } from "../types/servers";
 type ServerState = {
   serverMembers: IserverMember[];
   setServerMembers: (members: IserverMember[]) => void;
+  getServerMember: (memberId: string) => IserverMember | undefined;
 };
 
-export const useServerStore = create<ServerState>((set) => ({
+export const useServerStore = create<ServerState>((set, get) => ({
   serverMembers: [],
 
   setServerMembers: (members) => set({ serverMembers: members }),
+
+  getServerMember: (memberId) => {
+    const members = get().serverMembers;
+    return members.find((member) => member.id === memberId);
+  },
 }));
