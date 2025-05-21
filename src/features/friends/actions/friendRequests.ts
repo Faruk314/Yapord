749123192dfbafe5bd24f7a773faa02df9aaa61a
Faruk Spahic <farukspahictz@gmail.com>
2025-05-21 20:44:09 +0200

    Added actions for accepting and rejecting friend requests

--- src/features/friends/actions/friendRequests.ts
+++ src/features/friends/actions/friendRequests.ts
@@ -7,6 +7,8 @@ import { getUserByName } from "@/features/auth/db/user";
 import {
   checkFriendRequestExistance,
   insertFriendRequest,
+  acceptFriendRequest as acceptFriendRequestDb,
+  deleteFriendRequest as deleteFriendRequestDb,
 } from "../db/friendRequests";
 
 async function createFriendRequest(
@@ -49,6 +51,38 @@ async function createFriendRequest(
       error: true,
       message: "Server error. Could not create friend request",
     };
+  }
+}
+
+export async function acceptFriendRequest(senderId: string) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return { error: true, message: "Unauthorized" };
+  }
+
+  try {
+    await acceptFriendRequestDb(senderId, currentUser.id);
+
+    return { error: false, message: "Friend request accepted" };
+  } catch {
+    return { error: true, message: "Error accepting friend request" };
+  }
+}
+
+export async function rejectFriendRequest(senderId: string) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return { error: true, message: "Unauthorized" };
+  }
+
+  try {
+    await deleteFriendRequestDb(senderId, currentUser.id);
+
+    return { error: false, message: "Friend request rejected" };
+  } catch {
+    return { error: true, message: "Error rejecting friend request" };
   }
 }
 
