439ed417e1b9cd1e45b199105f25dc46b4169984
Faruk Spahic <farukspahictz@gmail.com>
2025-05-21 13:05:37 +0200

    Created all friends page

--- src/app/(private)/home/page.tsx
+++ src/app/(private)/home/page.tsx
@@ -1,22 +1,3 @@
-import { PrimaryBtn } from "@/components/buttons/PrimaryBtn";
-import { SecondaryBtn } from "@/components/buttons/SecondaryBtn";
-import { FaUserFriends } from "react-icons/fa";
-
 export default async function HomePage() {
-  return (
-    <div>
-      <div className="flex items-center space-x-4 h-18 px-4 border-b border-gray-300">
-        <div className="flex items-center space-x-2">
-          <FaUserFriends className="text-pink-600" size={25} />
-          <span className="text-sm font-black uppercase">friends</span>
-        </div>
-
-        <SecondaryBtn>Online</SecondaryBtn>
-
-        <SecondaryBtn>All</SecondaryBtn>
-
-        <PrimaryBtn>Add friend</PrimaryBtn>
-      </div>
-    </div>
-  );
+  return <div>Hej</div>;
 }
