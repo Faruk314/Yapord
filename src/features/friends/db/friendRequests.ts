d78f62b18f098fb11f3645cd97ee14c92e3ae9e4
Faruk Spahic <farukspahictz@gmail.com>
2025-05-21 20:43:30 +0200

    Added db method for getting friend requests, accepting and deleting

--- src/features/friends/db/friendRequests.ts
+++ src/features/friends/db/friendRequests.ts
@@ -1,7 +1,35 @@
 import { db } from "@/drizzle/db";
-import { FriendTable } from "@/drizzle/schema";
-import { revalidateFriendCache } from "../cache/friendRequests";
+import { FriendTable, UserTable } from "@/drizzle/schema";
+import {
+  getFriendRequestUserTag,
+  revalidateFriendRequestsCache,
+} from "../cache/friendRequests";
 import { and, eq, or } from "drizzle-orm";
+import { cacheTag } from "next/dist/server/use-cache/cache-tag";
+
+async function getFriendRequests(recipientId: string) {
+  "use cache";
+
+  cacheTag(getFriendRequestUserTag(recipientId));
+
+  const friendRequests = await db
+    .select({
+      id: UserTable.id,
+      name: UserTable.name,
+      email: UserTable.email,
+      image: UserTable.image,
+    })
+    .from(FriendTable)
+    .innerJoin(UserTable, eq(FriendTable.userAId, UserTable.id))
+    .where(
+      and(
+        eq(FriendTable.userBId, recipientId),
+        eq(FriendTable.status, "pending")
+      )
+    );
+
+  return friendRequests;
+}
 
 async function insertFriendRequest(data: typeof FriendTable.$inferInsert) {
   const [newFriendRequest] = await db
@@ -13,9 +41,46 @@ async function insertFriendRequest(data: typeof FriendTable.$inferInsert) {
   if (newFriendRequest == null)
     throw new Error("Failed to insert friend request");
 
-  revalidateFriendCache({ userId: newFriendRequest.userAId });
-
   return newFriendRequest;
+}
+
+export async function acceptFriendRequest(userAId: string, userBId: string) {
+  const [updatedFriend] = await db
+    .update(FriendTable)
+    .set({ status: "accepted" })
+    .where(
+      and(
+        eq(FriendTable.userAId, userAId),
+        eq(FriendTable.userBId, userBId),
+        eq(FriendTable.status, "pending")
+      )
+    )
+    .returning();
+
+  if (updatedFriend == null) throw new Error("Failed to accept friend request");
+
+  revalidateFriendRequestsCache({ userId: updatedFriend.userBId });
+
+  return updatedFriend;
+}
+
+export async function deleteFriendRequest(userAId: string, userBId: string) {
+  const [deletedFriend] = await db
+    .delete(FriendTable)
+    .where(
+      and(
+        eq(FriendTable.userAId, userAId),
+        eq(FriendTable.userBId, userBId),
+        eq(FriendTable.status, "pending")
+      )
+    )
+    .returning();
+
+  if (deletedFriend == null) throw new Error("Failed to delete friend request");
+
+  revalidateFriendRequestsCache({ userId: deletedFriend.userBId });
+
+  return deletedFriend;
 }
 
 async function checkFriendRequestExistance(
@@ -41,4 +106,4 @@ async function checkFriendRequestExistance(
   return friendRequest;
 }
 
-export { insertFriendRequest, checkFriendRequestExistance };
+export { getFriendRequests, insertFriendRequest, checkFriendRequestExistance };
