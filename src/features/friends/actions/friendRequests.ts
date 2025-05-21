dfd532c19c336c74f99f8ba294f57281a5b3e4b4
Faruk Spahic <farukspahictz@gmail.com>
2025-05-21 13:04:07 +0200

    Added create friend request action

---
+++ src/features/friends/actions/friendRequests.ts
@@ -0,0 +1,55 @@
+"use server";
+
+import { z } from "zod";
+import { friendRequestSchema } from "../schemas/friendRequest";
+import { getCurrentUser } from "@/features/auth/actions/user";
+import { getUserByName } from "@/features/auth/db/user";
+import {
+  checkFriendRequestExistance,
+  insertFriendRequest,
+} from "../db/friendRequests";
+
+async function createFriendRequest(
+  unsafeData: z.infer<typeof friendRequestSchema>
+) {
+  const { success, data } = friendRequestSchema.safeParse(unsafeData);
+
+  const currentUser = await getCurrentUser();
+
+  if (!success || !currentUser) {
+    return { error: true, message: "Invalid data or unauthorized" };
+  }
+
+  const recipient = await getUserByName(data.name);
+
+  if (!recipient) {
+    return { error: true, message: "User not found" };
+  }
+
+  if (currentUser.id === recipient.id)
+    return { error: true, message: "Could not create friend request" };
+
+  const requestExists = await checkFriendRequestExistance(
+    currentUser.id,
+    recipient.id
+  );
+
+  if (requestExists)
+    return { error: true, message: "Friend request already exists" };
+
+  try {
+    await insertFriendRequest({
+      userAId: currentUser.id,
+      userBId: recipient.id,
+    });
+
+    return { error: false, message: "Friend request successfully sent" };
+  } catch {
+    return {
+      error: true,
+      message: "Server error. Could not create friend request",
+    };
+  }
+}
+
+export { createFriendRequest };
