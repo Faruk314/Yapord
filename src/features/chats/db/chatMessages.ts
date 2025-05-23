a92863e03fc6002f801ae1066d98dafcad03e7e0
Faruk Spahic <farukspahictz@gmail.com>
2025-05-23 17:32:58 +0200

    Added db method for getting and inserting chat messages

---
+++ src/features/chats/db/chatMessages.ts
@@ -0,0 +1,41 @@
+"use server";
+
+import { db } from "@/drizzle/db";
+import { ChatMessagesTable } from "@/drizzle/schema";
+import { eq, asc } from "drizzle-orm";
+
+async function getChatMessages(chatId: string, page = 0) {
+  const limit = 50;
+  const offset = page * limit;
+
+  const messages = await db
+    .select({
+      id: ChatMessagesTable.id,
+      content: ChatMessagesTable.content,
+      createdAt: ChatMessagesTable.createdAt,
+      senderId: ChatMessagesTable.senderId,
+    })
+    .from(ChatMessagesTable)
+    .where(eq(ChatMessagesTable.chatId, chatId))
+    .orderBy(asc(ChatMessagesTable.createdAt))
+    .limit(limit)
+    .offset(offset);
+
+  const nextPage = messages.length === limit ? page + 1 : null;
+
+  return { messages, currentPage: page, nextPage };
+}
+
+async function insertChatMessage(data: typeof ChatMessagesTable.$inferInsert) {
+  const [newChatMessage] = await db
+    .insert(ChatMessagesTable)
+    .values(data)
+    .onConflictDoNothing()
+    .returning();
+
+  if (newChatMessage == null) throw new Error("Failed to insert chat message");
+
+  return newChatMessage;
+}
+
+export { getChatMessages, insertChatMessage };
