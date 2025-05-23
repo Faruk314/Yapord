623e0e006059b90d8541af2aed13f07a6fecac55
Faruk Spahic <farukspahictz@gmail.com>
2025-05-23 17:29:52 +0200

    Added db methods that get chat by key and insert chat

---
+++ src/features/chats/db/chats.ts
@@ -0,0 +1,29 @@
+import { db } from "@/drizzle/db";
+import { ChatTable } from "@/drizzle/schema";
+import { eq } from "drizzle-orm";
+
+async function getChatByKey(chatKey: string) {
+  return await db.query.ChatTable.findFirst({
+    where: eq(ChatTable.chatKey, chatKey),
+    columns: {
+      id: true,
+    },
+  });
+}
+
+async function insertChat(
+  data: typeof ChatTable.$inferInsert,
+  trx: Omit<typeof db, "$client">
+) {
+  const [newChat] = await trx
+    .insert(ChatTable)
+    .values(data)
+    .onConflictDoNothing()
+    .returning();
+
+  if (newChat == null) throw new Error("Failed to insert channel");
+
+  return newChat;
+}
+
+export { getChatByKey, insertChat };
