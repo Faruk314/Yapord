7c6abc1ddc3707c65e47e6e10ca1e95ba296d765
Faruk Spahic <farukspahictz@gmail.com>
2025-05-23 17:31:51 +0200

    Added method that initializes chat

---
+++ src/features/chats/actions/chats.ts
@@ -0,0 +1,43 @@
+"use server";
+
+import { getCurrentUser } from "@/features/auth/actions/user";
+import { createChatKey } from "../utils/chats";
+import { getChatByKey, insertChat as insertChatDb } from "../db/chats";
+import { redirect } from "next/navigation";
+import { db } from "@/drizzle/db";
+import { insertChatParticipants } from "../db/chatParticipants";
+import { Ichat } from "../types/chats";
+
+async function initializeChat(userB: string) {
+  const user = await getCurrentUser();
+
+  if (!user) {
+    return { error: true, message: "Unauthorized" };
+  }
+
+  const chatKey = createChatKey(user.id, userB);
+
+  const chat = await getChatByKey(chatKey);
+
+  if (chat) {
+    return redirect(`/home/chat/${chat.id}`);
+  }
+
+  let newChat: Ichat | null = null;
+
+  try {
+    await db.transaction(async (trx) => {
+      newChat = await insertChatDb({ chatKey }, trx);
+
+      const chatParticipants = [user.id, userB];
+
+      await insertChatParticipants(newChat.id, chatParticipants, trx);
+    });
+  } catch {
+    return { error: true, message: "Unable to create chat" };
+  }
+
+  redirect(`/home/chat/${newChat!.id}`);
+}
+
+export { initializeChat };
--- src/features/friends/components/Friend.tsx
+++ src/features/friends/components/Friend.tsx
@@ -1,7 +1,11 @@
+"use client";
+
 import { IconBtn } from "@/components/buttons/IconBtn";
 import Avatar from "@/components/ui/Avatar";
 import { Iuser } from "@/features/auth/types/user";
+import { initializeChat } from "@/features/chats/actions/chats";
 import { MessageCircle, MoreVertical } from "lucide-react";
+
 import React from "react";
 
 interface Props {
@@ -9,8 +13,12 @@ interface Props {
 }
 
 export default function Friend({ user }: Props) {
+  async function handleMessageClick() {
+    await initializeChat(user.id);
+  }
+
   return (
-    <div className="flex justify-between font-semibold max-w-[40rem] border border-gray-300 p-2 rounded-md">
+    <div className="flex justify-between border-b font-semibold max-w-[40rem] p-2 hover:bg-gray-100 duration-200 hover:rounded-md">
       <div className="flex space-x-2 items-center">
         <Avatar name={user.name} imageSrc={user.image} />
 
@@ -18,7 +26,11 @@ export default function Friend({ user }: Props) {
       </div>
 
       <div className="flex items-center rounded-md space-x-2">
-        <IconBtn icon={<MessageCircle />} className="h-10 w-10" />
+        <IconBtn
+          onClick={handleMessageClick}
+          icon={<MessageCircle />}
+          className="h-10 w-10"
+        />
         <IconBtn icon={<MoreVertical />} className="h-10 w-10" />
       </div>
     </div>
