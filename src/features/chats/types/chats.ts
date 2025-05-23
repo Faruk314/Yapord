0c120755684eda5213cbcc1d187d736e78a64e80
Faruk Spahic <farukspahictz@gmail.com>
2025-05-23 17:30:18 +0200

    Added chat type

---
+++ src/features/chats/types/chats.ts
@@ -0,0 +1,12 @@
+import { ChatType } from "@/drizzle/schema";
+
+interface Ichat {
+  name: string | null;
+  id: string;
+  type: ChatType;
+  createdAt: Date;
+  updatedAt: Date;
+  chatKey: string | null;
+}
+
+export type { Ichat };
