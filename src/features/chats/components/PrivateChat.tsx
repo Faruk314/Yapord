70ab92c3cd29a13f8dd3c4f88ada59e5a1842103
Faruk Spahic <farukspahictz@gmail.com>
2025-05-24 20:08:41 +0200

    Added privateChat component with reverse infinity scroll

---
+++ src/features/chats/components/PrivateChat.tsx
@@ -0,0 +1,125 @@
+"use client";
+
+import { IconBtn } from "@/components/buttons/IconBtn";
+import { Message } from "@/components/chat/Message";
+import { ChatInput } from "@/components/inputs/ChatInput";
+import { Iuser } from "@/features/auth/types/user";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { Upload } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { getChatMessages } from "../db/chatMessages";
+import { createChatMessage } from "../actions/chatMessages";
+import { chatMessageSchema } from "../schemas/chatMessage";
+import { toast } from "sonner";
+import InfiniteScroll from "react-infinite-scroll-component";
+
+interface Props {
+  chatId: string;
+  recipientInfo: Iuser;
+  senderInfo: Iuser;
+}
+
+export default function PrivateChat({
+  chatId,
+  recipientInfo,
+  senderInfo,
+}: Props) {
+  const { handleSubmit, reset, register } = useForm({
+    resolver: zodResolver(chatMessageSchema),
+    defaultValues: {
+      content: "",
+      chatId,
+      senderId: senderInfo.id,
+    },
+  });
+
+  const { data, status, fetchNextPage, isFetchingNextPage, hasNextPage } =
+    useInfiniteQuery({
+      queryKey: ["chatMessages", chatId],
+      queryFn: ({ pageParam = 0 }) => getChatMessages(chatId, pageParam),
+      initialPageParam: 0,
+      getNextPageParam: (lastPage) => lastPage.nextPage,
+      select: (data) => ({
+        pages: [...data.pages].reverse(),
+        pageParams: [...data.pageParams].reverse(),
+      }),
+    });
+
+  async function onSubmit(values: z.infer<typeof chatMessageSchema>) {
+    const data = await createChatMessage(values);
+
+    if (data.error) {
+      toast.error(data.message);
+
+      return;
+    }
+
+    reset();
+  }
+
+  console.log(data, "data");
+
+  return (
+    <div className="flex flex-col flex-1 min-h-0 bg-muted">
+      <div
+        id="chat-scroll"
+        className="flex-1 overflow-y-auto pt-4 pb-2 bg-muted"
+        style={{ display: "flex", flexDirection: "column-reverse" }}
+      >
+        <InfiniteScroll
+          dataLength={data?.pages.flatMap((p) => p.messages).length ?? 0}
+          next={() => {
+            if (!isFetchingNextPage) fetchNextPage();
+          }}
+          hasMore={hasNextPage ?? false}
+          inverse={true}
+          scrollableTarget="chat-scroll"
+          loader={<div className="text-center p-4">Loading...</div>}
+        >
+          {data?.pages
+            .flatMap((page) => page.messages)
+            .map((message, index, allMessages) => {
+              const previousMessage = allMessages[index - 1];
+              const isSameSenderAsPrevious =
+                previousMessage?.senderId === message.senderId;
+
+              const user =
+                message.senderId === recipientInfo.id
+                  ? recipientInfo
+                  : senderInfo;
+
+              return (
+                <Message
+                  key={message.id}
+                  isSameSenderAsPrevious={isSameSenderAsPrevious}
+                  senderName={user.name}
+                  imageSrc={user.image}
+                  createdAt={message.createdAt}
+                  message={message.content}
+                />
+              );
+            })}
+        </InfiniteScroll>
+      </div>
+
+      <div className="flex items-center rounded-md border border-gray-300 p-4 mx-4 mb-4 bg-white">
+        <form className="flex justify-between w-full">
+          <IconBtn type="button" className="h-8 w-8" icon={<Upload />} />
+          <ChatInput
+            placeholder={`Message ${recipientInfo.name}`}
+            className="flex-1 resize-none border-none self-center"
+            onKeyDown={(e) => {
+              if (e.key === "Enter" && !e.shiftKey) {
+                e.preventDefault();
+                handleSubmit(onSubmit)();
+              }
+            }}
+            {...register("content")}
+          />
+        </form>
+      </div>
+    </div>
+  );
+}
